94205d731ced1e29b01d8d98bd4dc9dfed05b715
rehne <rene.honnen@smail.th-koeln.de>
2017-04-25 20:05:20 +0200

    Aufgabe 2 | chalk angewendet

--- Kickoff_Aufgabe/Honnen/index.js
+++ Kickoff_Aufgabe/Honnen/index.js
@@ -4,13 +4,17 @@ var chalk = require('chalk');
 fs.readFile('staedte.json', function(err, data){
   var staedte = JSON.parse(data);
 
-  // Aufgabe 1: Formatierte Ausgabe auf der Konsole
+  /*
+   * Aufgabe 1: Formatierte Ausgabe auf der Konsole
+   * &&
+   * Aufgabe 2: bunte Ausgabe mit chalk.color()
+   */
   for(var i = 0; i < staedte.cities.length; i++){
-    console.log('name: ' + staedte.cities[i].name);
-    console.log('country: ' + staedte.cities[i].country);
-    console.log('population: ' + staedte.cities[i].population);
+    console.log(chalk.cyan('name: ' + staedte.cities[i].name));
+    console.log(chalk.green('country: ' + staedte.cities[i].country));
+    console.log(chalk.yellow('population: ' + staedte.cities[i].population));
     if(i < staedte.cities.length - 1){
-      console.log("--------------------");
+      console.log(chalk.red("--------------------"));
     }
   }
 });
