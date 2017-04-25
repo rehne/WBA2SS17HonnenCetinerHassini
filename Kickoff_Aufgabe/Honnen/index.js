924a0dfc64fa9f15bf5e17aadc4165784288b825
rehne <rene.honnen@smail.th-koeln.de>
2017-04-25 19:12:09 +0200

    Aufgabe 1 | formatierte Ausgabe

--- Kickoff_Aufgabe/Honnen/index.js
+++ Kickoff_Aufgabe/Honnen/index.js
@@ -1,5 +1,13 @@
 var fs = require('fs');
 
 fs.readFile('staedte.json', function(err, data){
-  console.log(data.toString());
+  var staedte = JSON.parse(data);
+
+  // Aufgabe 1: Formatierte Ausgabe auf der Konsole
+  for(var i = 0; i < staedte.cities.length; i++){
+    console.log('name: ' + staedte.cities[i].name);
+    console.log('country: ' + staedte.cities[i].country);
+    console.log('population: ' + staedte.cities[i].population);
+    console.log('--------------------');
+  };
 });
