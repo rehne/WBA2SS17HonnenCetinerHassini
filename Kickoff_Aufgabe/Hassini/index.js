4e0d6812d722a54300796046d0b4aaaf6f80dc2e
Sami <Sami@nat079038.nat.FH-Koeln.DE>
2017-05-04 14:54:49 +0200

    task 3

--- Kickoff_Aufgabe/Hassini/index.js
+++ Kickoff_Aufgabe/Hassini/index.js
@@ -7,7 +7,14 @@ fs.readFile( __dirname + '/staedte.json', function(err, data) {
         throw err;
     }
     else{
+                                          
         data = JSON.parse(data)
+        data.cities.sort(function (a, b) {
+        return a.population - b.population;
+});
+        fs.writeFile(__dirname+"/staedte_sorted.json", JSON.stringify(data));
+                                                                 
+                               
         for(i = 0; data.cities[i]; i++){
         console.log(chalk.green("name: " + data.cities[i].name));
         console.log(chalk.red("country: " + data.cities[i].country));
