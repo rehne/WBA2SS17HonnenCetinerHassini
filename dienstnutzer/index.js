1b63d6e16c0f9fad18ba8292ccd1a9a74b0f83d2
rehne <rene.honnen@smail.th-koeln.de>
2017-07-03 16:31:17 +0200

    udpate

--- dienstnutzer/index.js
+++ dienstnutzer/index.js
@@ -1,1 +1,21 @@
 var http = require('http');
+var express = require('express');
+var request = require('request');
+var app = express();
+
+var dHost = 'http://localhost';
+var dPort = 3000;
+var dUrl = dHost + ':' + dPort;
+
+app.get('/users', function(req, res){
+  var url = dUrl + '/users';
+
+  request(url, function(err, response, body){
+    body = JSON.parse(body);
+    res.json(body);
+  });
+});
+
+app.listen(3001, function(){
+  console.log('Dienstnutzer läuft auf Port 3001.');
+});
